2ef42edbe01954a64733c042f01daf12a6f0681d
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-20 16:00:29 +0545

    feat: Added user form and capture data from from

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./app/components/Navbar";
+import Navbar from "./components/Navbar";
 import "./App.css";
-Navbar;
+import Create from "./components/Create";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Create />}></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/components/Create.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Create = () => {
+  const [users, setUsers] = useState({});
+
+  const getUserData = (e) => {
+    setUsers({ ...users, [e.target.name]: e.target.value });
+    console.log(users);
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <form className="w-50 mx-auto my-5" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="form-label">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            onChange={getUserData}
+          />
+          <div id="emailHelp" className="form-text"></div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="form-label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            onChange={getUserData}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="form-label">Age</label>
+          <input
+            type="number"
+            name="age"
+            className="form-control"
+            onChange={getUserData}
+          />
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            value="male"
+            name="gender"
+            onChange={getUserData}
+          />
+          <label className="form-check-label" htmlFor="form-check-label">
+            Male
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            value="female"
+            name="gender"
+            onChange={getUserData}
+          />
+          <label className="form-check-label" htmlFor="flexCheckChecked">
+            Female
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
